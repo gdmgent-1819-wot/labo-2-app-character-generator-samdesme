5ff06bae6d6fe80661d9d5be255e40b29d1c9d16
“samdesme” <samdesme@student.arteveldehs.be>
2018-10-04 00:48:35 +0200

    python finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,25 +18,25 @@ var ArrPixel = [];
 
 for(let i = 0; i < pixels.length; i++) {
 
-  ArrPixel.push([255,255,255]);
+  ArrPixel.push("0");
   var pixel = pixels[i];
   pixel.addEventListener('click', e => {
 
     switch (penColour) {
       case "red":
-        ArrPixel[i] = [255, 0, 0];
+        ArrPixel[i] = "rd";
         break;
         case "chartreuse":
-        ArrPixel[i] = [0, 255, 0];
+        ArrPixel[i] = "gr";
         break;
         case "blue":
-        ArrPixel[i] = [0, 0, 255];       
+        ArrPixel[i] = "bl";       
         break;
         case "black":
-        ArrPixel[i] = [0, 0, 0];
+        ArrPixel[i] = "blk";
         break
         default: 
-        ArrPixel[i] = [255, 255, 255];
+        ArrPixel[i] = "0";
     }
   });
 }
--- pi/app_char_generator.py
+++ pi/app_char_generator.py
@@ -10,12 +10,74 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 # constants
-COLOR_RED = (255, 0, 0)
-COLOR_GREEN = (0, 255, 0)
-COLOR_BLUE = (0, 0, 255)
-COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-
-serviceAccountKey = "./../../keys/arcadekey.json"
+serviceAccountKey = '../wot-labo2-samdesme-firebase-adminsdk-53u5f-93c77bb6c6.json'
 databaseURL = "https://wot-labo2-samdesme.firebaseio.com/"
+
+try:
+    firebase_cred = credentials.Certificate(serviceAccountKey)
+    firebase_admin.initialize_app(firebase_cred, {
+    'databaseURL': databaseURL
+    })
+    firebase_ref_values = db.reference('values')
+
+except:
+    print('Unable to initialize Firebase: {}'.format(sys.exc_info()[0]))
+    sys.exit(1)
+
+
+def get_values():
+    colorVal = firebase_ref_values.get()
+    i = 0
+    char = []
+    if colorVal is not None:
+        for item, value in colorVal.items():
+            print(value)
+            for val in range(0, 64):
+                letter = value[val]
+                if letter == "rd":
+                    color = RED
+                elif letter == "gr":
+                    color = GREEN
+                elif letter == "bl":
+                    color = BLUE
+                elif letter == "blk":
+                    color = BLACK
+                else:
+                    color = WHITE
+                            
+                char.append(color)
+                  
+        sense_hat.set_pixels(char)
+        sleep(1)
+           
+
+    else:
+        sense_hat.show_message("404")
+try:
+    # SenseHat
+    sense_hat = SenseHat()
+    sense_hat.set_imu_config(False, False, False)
+except:
+    print('Unable to initialize the Sense Hat library: {}'.format(sys.exc_info()[0]))
+    sys.exit(1)
+    
+def main():
+    while True:
+        get_values()
+            
+        
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit):
+        print('Interrupt received! Stopping the application...')
+    finally:
+        print('Cleaning up the mess...')
+        sense_hat.clear()
+        sys.exit(0)
